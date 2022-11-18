cce71e1a49d64a5d2906e553a5efaae4efa7fec0
duc do <tvietduc122@gmail.com>
2022-11-18 22:59:00 +0700

    html index page

--- src/pages/IndexPage.js
+++ src/pages/IndexPage.js
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from 'react';
 
 function IndexPage() {
-  return (
-    <div>IndexPage</div>
-  )
+    return (
+        <div className="register-page">
+            <div className="register-box">
+                <div className="register-logo">
+                    <a href="https://github.com">
+                        <b>Admin</b>LTE
+                    </a>
+                </div>
+                <div className="card">
+                    <div className="card-body register-card-body">
+                        <p className="login-box-msg">Index Page</p>
+
+                        <div className="row">
+                            <div className="col-6">
+                                <a
+                                    href="/register.html"
+                                    className="btn btn-primary btn-block"
+                                >
+                                    Register
+                                </a>
+                            </div>
+                            <div className="col-6">
+                                <a
+                                    href="/login.html"
+                                    className="btn btn-primary btn-block"
+                                >
+                                    Login
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
-export default IndexPage
+export default IndexPage;
