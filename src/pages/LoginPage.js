f6be96a30538915b01b88acaa7197677238fe679
duc do <tvietduc122@gmail.com>
2022-11-19 07:18:55 +0700

    component login and register

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,9 +1,95 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+
 
 function LoginPage() {
-  return (
-    <div>LoginPage</div>
-  )
+    return (
+        <div className="login-page">
+            <div className="login-box">
+                <div className="login-logo">
+                    <a href="https://github.com">
+                        <b>Admin</b>LTE
+                    </a>
+                </div>
+                <div className="card">
+                    <div className="card-body login-card-body">
+                        <p className="login-box-msg">
+                            Sign in to start your session
+                        </p>
+
+                        <form>
+                            <div className="input-group mb-3">
+                                <input
+                                    id="email"
+                                    type="email"
+                                    className="form-control"
+                                    placeholder="Email"
+                                />
+                                <div className="input-group-append">
+                                    <div className="input-group-text">
+                                        <span className="fas fa-envelope"></span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="input-group mb-3">
+                                <input
+                                    id="password"
+                                    type="password"
+                                    className="form-control"
+                                    placeholder="Password"
+                                />
+                                <div className="input-group-append">
+                                    <div className="input-group-text">
+                                        <span className="fas fa-lock"></span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="col-4">
+                                    <button
+                                        type="submit"
+                                        className="btn btn-primary btn-block"
+                                    >
+                                        Sign In
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+
+                        <div className="social-auth-links text-center mb-3">
+                            <p>- OR -</p>
+                            <a
+                                href="https://github.com"
+                                className="btn btn-block btn-primary"
+                            >
+                                <i className="fab fa-facebook mr-2"></i> Sign in
+                                using Facebook
+                            </a>
+                            <a
+                                href="https://github.com"
+                                className="btn btn-block btn-danger"
+                            >
+                                <i className="fab fa-google-plus mr-2"></i> Sign
+                                in using Google+
+                            </a>
+                        </div>
+
+                        <p className="mb-1">
+                            <a href="https://github.com">
+                                I forgot my password
+                            </a>
+                        </p>
+                        <p className="mb-0">
+                            <Link to="/register" className="text-center">
+                                Register a new membership
+                            </Link>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
-export default LoginPage
+export default LoginPage;
--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -1,9 +1,101 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 function RegisterPage() {
-  return (
-    <div>RegisterPage</div>
-  )
+    return (
+        <div className="register-page">
+            <div className="register-box">
+                <div className="register-logo">
+                    <a href="https://github.com">
+                        <b>Code</b>Love
+                    </a>
+                </div>
+
+                <div className="card">
+                    <div className="card-body register-card-body">
+                        <p className="login-box-msg">
+                            Register a new membership
+                        </p>
+
+                        <form>
+                            <div className="input-group mb-3">
+                                <input
+                                    id="username"
+                                    type="text"
+                                    className="form-control"
+                                    placeholder="User name"
+                                />
+                                <div className="input-group-append">
+                                    <div className="input-group-text">
+                                        <span className="fas fa-user"></span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="input-group mb-3">
+                                <input
+                                    id="email"
+                                    type="email"
+                                    className="form-control"
+                                    placeholder="Email"
+                                />
+                                <div className="input-group-append">
+                                    <div className="input-group-text">
+                                        <span className="fas fa-envelope"></span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="input-group mb-3">
+                                <input
+                                    id="password"
+                                    type="password"
+                                    className="form-control"
+                                    placeholder="Password"
+                                />
+                                <div className="input-group-append">
+                                    <div className="input-group-text">
+                                        <span className="fas fa-lock"></span>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="row">
+                                <div className="col-4">
+                                    <button
+                                        type="submit"
+                                        className="btn btn-primary btn-block"
+                                    >
+                                        Register
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+
+                        <div className="social-auth-links text-center">
+                            <p>- OR -</p>
+                            <a
+                                href="https://github.com"
+                                className="btn btn-block btn-primary"
+                            >
+                                <i className="fab fa-facebook mr-2"></i>
+                                Sign up using Facebook
+                            </a>
+                            <a
+                                href="https://github.com"
+                                className="btn btn-block btn-danger"
+                            >
+                                <i className="fab fa-google-plus mr-2"></i>
+                                Sign up using Google+
+                            </a>
+                        </div>
+
+                        <Link to="/login" className="text-center">
+                            I already have a membership
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
-export default RegisterPage
+export default RegisterPage;
